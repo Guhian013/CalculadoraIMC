ec3ebd1260afe8ff07e739f8b442cee780881ead
Gustavo Ian <gustavoian1790@gmail.com>
2022-03-30 11:00:25 -0300

    IMC Calculator Working (First Commit)

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Dia, bom");
+﻿Console.Write("Entre com o seu nome: ");
+string nome = Console.ReadLine();
+
+Console.Write("Entre com a sua altura. Ex: 1,65: ");
+float altura = Convert.ToSingle(Console.ReadLine());
+
+Console.Write("Entre com o seu peso em kg. Ex: 67,5: ");
+float peso = Convert.ToSingle(Console.ReadLine());
+
+double imc = peso / (altura * altura);
+
+Console.WriteLine($"\n{nome}, sua altura é {altura} e seu peso {peso}");
+Console.WriteLine($"Seu IMC é {imc}");
