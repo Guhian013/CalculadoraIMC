deb2daa3304446abca4c2f02316c39df760f62dd
Gustavo Ian <gustavoian1790@gmail.com>
2022-04-07 14:00:18 -0300

    BMI check to set user rating

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿Console.Write("Entre com o seu nome: ");
-string nome = Console.ReadLine();
+﻿string situacao;
+double imc;
+
+Console.Write("\nEntre com o seu nome: ");
+string nome = Console.ReadLine()!;
 
 Console.Write("Entre com a sua altura. Ex: 1,65: ");
 float altura = Convert.ToSingle(Console.ReadLine());
@@ -7,7 +10,54 @@ float altura = Convert.ToSingle(Console.ReadLine());
 Console.Write("Entre com o seu peso em kg. Ex: 67,5: ");
 float peso = Convert.ToSingle(Console.ReadLine());
 
-double imc = peso / (altura * altura);
+if (altura <= 00.00 || peso <= 00.00)
+{
+    Console.Clear();
+    Console.WriteLine("\nPor favor, não insira valores negativos ou nulos!");
+}
+else
+{
+    imc = peso / (altura * altura);
 
-Console.WriteLine($"\n{nome}, sua altura é {altura} e seu peso {peso}");
-Console.WriteLine($"Seu IMC é {imc}");
+    verificarIMC();
+
+    Console.Clear();
+    Console.WriteLine($"\n{nome}, sua altura é {altura} e seu peso {peso}");
+    Console.WriteLine($"Seu IMC é {Math.Round(imc,2)}, isso significa que você está {situacao}");
+}
+
+string verificarIMC() 
+{
+    if (imc < 17) 
+    {
+        return situacao = "muito abaixo do peso.";
+    } 
+    else if (imc >= 17 && imc < 18.50)
+    {
+        return situacao = "abaixo do peso.";
+    }
+    else if (imc >= 18.50 && imc < 25)
+    {
+        return situacao = "com um peso normal.";
+    }
+    else if (imc >= 25 && imc < 30)
+    {
+        return situacao = "acima do peso.";
+    }
+    else if (imc >= 30 && imc < 35)
+    {
+       return situacao = "com um nível de obesidade I.";
+    }
+    else if (imc >= 35 && imc < 40)
+    {
+        return situacao = "com um nível de obesidade II (severa).";
+    }
+    else if (imc > 40)
+    {
+        return situacao = "com um nível de obesidade III (mórbida).";
+    }
+    else
+    {
+        return situacao = "";
+    }
+}
--- README.md
+++ README.md
@@ -4,13 +4,18 @@ Aplicação console para cálculo do IMC.
 
 ## Funcionalidades
 - Calcula e apresenta o IMC
+- Mostra ao usuário em que faixa do IMC ele se encontra
 
 ## Tecnologias Utilizadas
-- dotnet
+- SDK do .NET 6.0.201
 
 ## Como Exacutar
-- Realize o clone do repositório e execute o comando:
+- Realize o clone do repositório com este comando:
 
+```
+git clone https://github.com/Guhian013/CalculadoraIMC
+```
+- e execute a calculadora de IMC com este comando:
 ```
 dotnet run
 ```
